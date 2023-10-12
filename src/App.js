5b2d2c0203d11e7186f661e600aa5a4422c905b5
RutvikJogdand <jogdandrutvik7@gmail.com>
2023-10-13 03:07:45 +0530

    made download route and main route. Styled the main chart page to output required pdf

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './componets/MainPage/MainPage';
+import AppRoutes from './routes/Routes';
+
+function App() {
+  return (
+    <div className="App">
+     <AppRoutes/>
+    </div>
+  );
+}
+
+export default App;
